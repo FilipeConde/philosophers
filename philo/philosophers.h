c0fa396875023af811d19ac26bba079d7e4f2bb6
Filipe Conde <fconde-p@student.42sp.org.br>
2026-06-13 18:37:06 -0300

    chore: cria estruturas para philo e simulação.

--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 15:32:44 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/06/13 15:32:46 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/06/13 18:35:46 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,39 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <sys/time.h>
+# include <pthread.h>
+
+typedef struct s_sim
+{
+	int				philo_qty;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
+	long			max_meals;
+	unsigned long	start_time;
+	int				stop_sim_flag;
+	pthread_mutex_t	print_lock;
+	pthread_mutex_t	stop_sim_lock;
+	pthread_mutex_t	*forks;
+	pthread_t		*thread_id;
+	t_philo			*philos;
+}	t_sim;
+
+typedef struct s_philo
+{
+	int				index;
+	unsigned long	start_time;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*r_fork;
+	unsigned long	time_last_meal;
+	long			meal_count;
+	long			time_to_eat;
+	long			time_to_sleep;
+	pthread_mutex_t	philo_lock;
+	pthread_mutex_t	*print_lock;
+	pthread_mutex_t	*stop_sim_lock;
+	int				*stop_sim;
+}	t_philo;
 
 int				sample(void);
 unsigned long	get_current_time(void);
