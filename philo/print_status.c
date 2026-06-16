6880a3b78d158f7328de6b5264b8d20fa3b01dfb
Filipe Conde <fconde-p@student.42sp.org.br>
2026-06-16 04:56:43 -0300

    chore: cria função para imprimir status dos filósofos.

--- philo/print_status.c
+++ philo/print_status.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 04:34:23 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/06/16 04:42:42 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/06/16 04:54:33 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	print_status(t_philo *philo, char *str)
 	curr_time = 0;
 	pthread_mutex_lock(philo->print_lock);
 	curr_time = get_current_time();
-	if (has_sim_stoped == 0)
+	if (has_sim_stoped(philo->stop_sim, philo->stop_sim_lock) == 0)
 		printf("%lu %d %s\n", curr_time - philo->start_time, philo->index, str);
 	pthread_mutex_unlock(philo->print_lock);
 }
